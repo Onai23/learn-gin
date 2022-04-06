c39c9f474494d109ff57067fcc6cd58a0e024908
Onai23 <onai.nadapdap.23@gmail.com>
2022-04-06 08:08:44 +0700

    Modifikasi file main.go, menambahkan handler GET

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+func main() {
+	//set default route
+	r := gin.Default()
+
+	//set handler
+	//gin.Context = membawa detail permintaan, memvalidasi dan membuat serialisasi data json
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "berhasil akses home",
+		})
+	})
+	//menjalakan server
+	r.Run("localhost:8080")
 }
