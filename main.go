1f4d3b92c862257f2f440986450eb08171331a82
Onai23 <onai.nadapdap.23@gmail.com>
2022-04-06 10:01:58 +0700

    Menambahkan fungsi getArticle() pada main.go

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	//set default route
@@ -9,6 +11,7 @@ func main() {
 	//set handler
 	//gin.Context = membawa detail permintaan, memvalidasi dan membuat serialisasi data json
 	router.GET("/", getHome)
+	router.GET("/article/:title", getArticle)
 	//menjalakan server
 	router.Run("localhost:8080")
 }
@@ -16,8 +19,20 @@ func main() {
 //set fungsi getHome() untuk route "/"
 //gin.Context = membawa detail permintaan, memvalidasi dan membuat serialisasi data json
 func getHome(c *gin.Context) {
+	//gin.H = set response
 	c.JSON(200, gin.H{
 		"status":  "Berhasil",
 		"message": "Berhasil akses home",
+	})
+}
+
+//set fungsi getArticle()
+func getArticle(c *gin.Context) {
+	//get parameter title
+	title := c.Param("title")
+	//set response dari parameter
+	c.JSON(200, gin.H{
+		"status":  "Berhasil",
+		"message": title,
 	})
 }
