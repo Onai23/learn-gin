1236da1fec13c094e6a0013315888469e4f3041b
Onai23 <onai.nadapdap.23@gmail.com>
2022-04-06 10:18:46 +0700

    Menambahkan fungsi postArticle() pada main.go

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	//gin.Context = membawa detail permintaan, memvalidasi dan membuat serialisasi data json
 	router.GET("/", getHome)
 	router.GET("/article/:title", getArticle)
+	router.POST("/articles", postArticle)
 	//menjalakan server
 	router.Run("localhost:8080")
 }
@@ -34,5 +35,19 @@ func getArticle(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"status":  "Berhasil",
 		"message": title,
+	})
+}
+
+//set fungsi postArticle()
+func postArticle(c *gin.Context) {
+	//mengambil data yang dikirim dari form post, kirim dari url-form-encoded
+	title := c.PostForm("title")
+	desc := c.PostForm("desc")
+
+	//set response json
+	c.JSON(200, gin.H{
+		"title":  title,
+		"desc":   desc,
+		"status": "Berhasil ngepost",
 	})
 }
