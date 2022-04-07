de37018a7571620665a8e5432a2e5f6ae3e68d24
Onai23 <onai.nadapdap.23@gmail.com>
2022-04-07 09:04:54 +0700

    Menambahkan handler pada server.go dan menambahkan file auth.go di routes dan config folder

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/gin-gonic/gin"
+	"github.com/subosito/gotenv"
 	"learn-gin/config"
 	"learn-gin/routes"
 )
@@ -10,11 +11,17 @@ import (
 func main() {
 	config.InitDB()
 	defer config.DB.Close()
+	//memanggil method dari gotenv
+	gotenv.Load()
 	//set default route
 	router := gin.Default()
 	//set group dari router ke /api/vi
 	v1 := router.Group("/api/v1")
 	{
+		//menambahkan path parameter /auth/:provider
+		v1.GET("auth/:provider", routes.RedirectHandler)
+		v1.GET("/auth/:provider/callback", routes.CallbackHandler)
+
 		//membuat route khusus menangani article, -> ke home, post article, get article
 		articles := v1.Group("/articles")
 		{
