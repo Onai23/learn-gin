1c26328f6c8b08956120bbb4c7f8df75e7352953
Onai23 <onai.nadapdap.23@gmail.com>
2022-04-07 10:10:55 +0700

    Menambahkan file user.go, untuk model table users

---
+++ models/user.go
@@ -0,0 +1,18 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+//Model, dibentuk database orm, mewakili tabel2 yang ada di database
+//id unique akan otomatis dibuatkan
+//deklarasi cetakan User, user dapat memiliki lebih dari 1 article
+type User struct {
+	gorm.Model
+	Articles []Article
+	Username string
+	FullName string
+	Email    string
+	SocialId string
+	Provider string
+	Avatar   string
+	Role     bool `gorm:"default:0"`
+}
